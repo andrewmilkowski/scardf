45944111b494c0f4e04a678f67f910dd344a7f24
Andrew Milkowski <andrewmilkowski@gmail.com>
2010-07-30 12:52:15 -0400

    maven install script

---
+++ install-pom.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -DgroupId=net.croz.scardf -DartifactId=scardf -Dversion=0.3-SNAPSHOT -Dpackaging=jar -Dfile=./target/scala_2.8.0/scardf_2.8.0-0.3-SNAPSHOT.jar -DgeneratePom=true -DpomFile=pom.xml
+
