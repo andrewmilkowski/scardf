9ca833eb4b6696f7e54f8594b44a4c70e2a88302
Andrew Milkowski <andrewmilkowski@gmail.com>
2010-07-29 23:26:52 -0400

    sbt fixes to jena and arq versions, work in progress

--- project/build/ScardfProject.scala
+++ project/build/ScardfProject.scala
@@ -2,12 +2,13 @@ import sbt._
 
 class ScardfProject(info: ProjectInfo) extends DefaultProject(info) {
 
-lazy val JENA_VERSION  = "2.6.2"
-lazy val ARQ_VERSION  = "2.8.3"
+lazy val JENA_VERSION  = "2.6.3"
+lazy val ARQ_VERSION  = "2.8.4"
 lazy val JODATIME_VERSION  = "1.6"
 lazy val COMMONS_LOGGING_VERSION  = "1.1.1"
 lazy val SCALA_SPECS_VERSION  = "1.6.2"
 lazy val SCALA_CHECK_VERSION  = "1.7"
+lazy val JUNIT_VERSION = "4.7"
 
 // Compiler settings
 
@@ -23,7 +24,7 @@ lazy val SCALA_CHECK_VERSION  = "1.7"
  // Repositories
 
   lazy val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
-  lazy val mavenRemote = "Remote Maven Repository" at "http://repo1.maven.org/maven2"
+  lazy val mavenRemote = "Remote Maven Repository" at "http://repo2.maven.org/maven2"
 
 // Dependencies (Compile)
 
@@ -34,7 +35,7 @@ lazy val SCALA_CHECK_VERSION  = "1.7"
 
 // Dependencies (Test)
 
-  lazy val junit = "junit" % "junit" % "4.7" % "test->default"
+  lazy val junit = "junit" % "junit" % JUNIT_VERSION % "test->default"
   lazy val scalaSpecs = "org.scala-tools.testing" % "specs_2.8.0.Beta1-RC8" % SCALA_SPECS_VERSION % "test->default"
   lazy val scalaCheck = "org.scala-tools.testing" % "scalacheck_2.8.0.RC7" % SCALA_CHECK_VERSION % "test->default"
 }
